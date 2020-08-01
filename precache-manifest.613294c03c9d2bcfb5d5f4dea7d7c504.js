01f29d2a1940d2a1feb6796180a788bb3dfed00a
shrikant21 <55912627+shrikant21@users.noreply.github.com>
2020-08-01 11:04:55 +0530

    Updates

---
+++ precache-manifest.613294c03c9d2bcfb5d5f4dea7d7c504.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ba203083e53376a6180",
+    "url": "/ChitChat.github.io/static/css/main.14a6a8e9.chunk.css"
+  },
+  {
+    "revision": "3ba203083e53376a6180",
+    "url": "/ChitChat.github.io/static/js/main.9c6083bb.chunk.js"
+  },
+  {
+    "revision": "d203afa7768cbc33e579",
+    "url": "/ChitChat.github.io/static/js/runtime~main.91e45b2d.js"
+  },
+  {
+    "revision": "75036c7ad193bfedbc37",
+    "url": "/ChitChat.github.io/static/js/2.ab73fbb6.chunk.js"
+  },
+  {
+    "revision": "01252401c0e012846fdd1840258ef6bd",
+    "url": "/ChitChat.github.io/index.html"
+  }
+];
